fdc0d13cdbedee47e48bdb6fa75e4017cf77a97f
adithyarajendran <72382374+adithyarajendran@users.noreply.github.com>
2020-12-10 20:53:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+ 
+var engine,world;
+var ground
+function setup(){
+
+createCanvas(3000,800)
+engine = Engine.create();
+world = engine.world;
+ground = new Ground(600,600,1200,20)
+
+}
+
+function draw(){
+
+ground.display()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
